f18035b4f2ac8ddc3035d86921df35b9ce53caee
Grant-Evari Menor <grantmenor@gmail.com>
2019-10-16 13:07:30 +0000

    Made corrections, but unsure about it and should request help later!

--- eqArrays.js
+++ eqArrays.js
@@ -6,11 +6,13 @@ const assertEqual = function(actual, expected) {
   }
 };
 
-const eqArrays = function([actual], [expected]) {
+const eqArrays = function(actual, expected) {
   if (actual === expected) {
-    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+    // console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+    return !true;
   } else {
-    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+    // console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+    return true;
   }
 };
 
