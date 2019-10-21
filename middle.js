162928947d3c56f69a77f09cc3e6b9af58517b49
Grant-Evari Menor <grantmenor@gmail.com>
2019-10-21 20:21:12 +0000

    middle.js has been exported

--- middle.js
+++ middle.js
@@ -1,39 +1,5 @@
-const assertArraysEqual = function(array1, array2) {
-  if (array1.length === array2.length) {
-    for (let i = 0; i < array1.length; i++) {
-      if (array1[i] !== array2[i]) {
-        console.log(`🛑🛑🛑 Assertion Failed: ${array1} !== ${array2}`)
-        return false;
-      }
-    }
-  } else {
-    console.log(`🛑🛑🛑 Assertion Failed: ${array1} !== ${array2}`)
-    return false;
-  }
-  console.log(`✅✅✅ Assertion Passed: ${array1} === ${array2}`)
-  return true;
-};
-
-
-const eqArrays = function(array1, array2) {
-  if (array1.length === array2.length) {
-    for (let i = 0; i < array1.length; i++) {
-      if (array1[i] === array2[i]) {
-        return true;
-      }
-    }
-  } else {
-    return false;
-  }
-  return true;
-};
-
-
-
 const middle = function(array) {
   let middleElements = [];
-
-  
   if (array.length <= 2) {
     return [];
   } else if (array.length % 2 === 0) {
@@ -47,9 +13,5 @@ const middle = function(array) {
   }
 }
 
-assertArraysEqual(middle([1]), []); // => []
-assertArraysEqual(middle([1, 2]), []); // => []
-assertArraysEqual(middle([1, 2, 3]), [2]); // => [2]
-assertArraysEqual(middle([1, 2, 3, 4, 5]), [3]); // => [3]
-assertArraysEqual(middle([1, 2, 3, 4]), [2, 3]); // => [2, 3]
-assertArraysEqual(middle([1, 2, 3, 4, 5, 6]), [3, 4]); // => [3, 4]
+
+module.exports = middle;
