7d989b9843b4b525d08712b5476141f21c26adee
Grant-Evari Menor <grantmenor@gmail.com>
2019-10-21 19:36:49 +0000

    head.js has been exported

--- head.js
+++ head.js
@@ -1,15 +1,8 @@
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
-
 const head = function(number) {
-  return number[0]
+  if (number.length === 0) {
+    return undefined;
+  }
+  return number[0];
 }
 
-
-assertEqual(head([5,6,7]), 5);
-assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
+module.exports = head;
