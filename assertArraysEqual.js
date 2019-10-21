3cbff82b9577c3e29e52bb7b6eb61a8e5ce4b32c
Grant-Evari Menor <grantmenor@gmail.com>
2019-10-21 20:20:40 +0000

    assertArraysEqual has been exported

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -14,8 +14,11 @@ const assertArraysEqual = function(array1, array2) {
   return true;
 };
 
-assertArraysEqual([1, 2, 3], [1, 2, 3]); // => true
-assertArraysEqual([1, 2, 3], [3, 2, 1]); // => false
+// assertArraysEqual([1, 2, 3], [1, 2, 3]); // => true
+// assertArraysEqual([1, 2, 3], [3, 2, 1]); // => false
 
-assertArraysEqual(["1", "2", "3"], ["1", "2", "3"]); // => true
-assertArraysEqual(["1", "2", "3"], ["1", "2"]); //=> false
+// assertArraysEqual(["1", "2", "3"], ["1", "2", "3"]); // => true
+// assertArraysEqual(["1", "2", "3"], ["1", "2"]); //=> false
+
+
+module.exports = assertArraysEqual;
