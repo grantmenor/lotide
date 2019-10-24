c3847b51701eb900c1451283d8195debed0fece6
Grant-Evari Menor <grantmenor@gmail.com>
2019-10-24 20:21:56 +0000

    Redone!

--- eqArrays.js
+++ eqArrays.js
@@ -1,3 +1,6 @@
+const assertEqual = require('./assertEqual');
+
+
 const eqArrays = function(array1, array2) {
   if (array1.length === array2.length) {
     for (let i = 0; i < array1.length; i++) {
